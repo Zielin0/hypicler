21ee16ebc9910c125f1288ebbc78ffc37fc0562a
Zielin0 <me@zielinus.xyz>
2023-08-01 15:48:42 +0200

    feat: social links on player card

--- app/(stats)/player/[username]/page.tsx
+++ app/(stats)/player/[username]/page.tsx
@@ -1,19 +1,38 @@
 'use client';
 
 import PlayerProfileCard from '@/app/components/PlayerProfileCard';
-import { createStyles } from '@mantine/core';
+import { createStyles, Notification, rem } from '@mantine/core';
+import { IconCheck } from '@tabler/icons-react';
 import { Session } from 'hypicle';
+import { useState } from 'react';
 
 const useStyles = createStyles((theme) => ({
   container: {
     display: 'flex',
     alignItems: 'left',
   },
+  notification: {
+    position: 'fixed',
+    bottom: rem(100),
+    right: theme.spacing.md,
+    [theme.fn.smallerThan('xs')]: {
+      bottom: rem(85),
+      right: rem(5),
+    },
+  },
 }));
 
 // TODO: Fetch and apply data from the API
 export default function Page({ params }: { params: { username: string } }) {
   const { classes, cx } = useStyles();
+  const [showNotif, setShowNotif] = useState(false);
+
+  const handleCopy = () => {
+    setShowNotif(true);
+    setTimeout(() => {
+      setShowNotif(false);
+    }, 5000);
+  };
 
   return (
     <div className={classes.container}>
@@ -25,8 +44,32 @@ export default function Page({ params }: { params: { username: string } }) {
         karma={1003090}
         status={{ online: false } as Session}
         rank={'VIP'}
-        socials={{ links: { DISCORD: 'Zielino#7342' }, prompt: true }}
+        socials={{
+          links: {
+            DISCORD: 'Zielino#7342',
+            HYPIXEL: 'https://hypixel.net/members/zielino.5132535',
+            TWITCH: 'https://twitch.tv/zielin0',
+            TIKTOK: 'https://tiktok.com/@wdklfujhqlwekiufhk',
+            INSTAGRAM: 'https://instagram.com/asdkujfhqlkwrg',
+            YOUTUBE: 'https://youtube.com/@ZielinoCoding',
+            TWITTER: 'https://twitter.com/theZielino',
+          },
+          prompt: true,
+        }}
+        onCopy={handleCopy}
       />
+      {showNotif && (
+        <Notification
+          className={classes.notification}
+          icon={<IconCheck size="1.1rem" />}
+          onClose={() => setShowNotif(false)}
+          color="teal"
+          title="Success!"
+          withBorder
+        >
+          Copied Discord to the Clipboard
+        </Notification>
+      )}
     </div>
   );
 }
--- app/components/PlayerProfileCard.tsx
+++ app/components/PlayerProfileCard.tsx
@@ -1,28 +1,75 @@
 import {
+  ActionIcon,
   Badge,
   Card,
+  CopyButton,
   Group,
   Image,
+  Popover,
   Space,
   Text,
   createStyles,
   rem,
 } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import {
+  IconBrandDiscord,
+  IconBrandInstagram,
+  IconBrandTiktok,
+  IconBrandTwitch,
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconHexagonLetterH,
+} from '@tabler/icons-react';
 import dayjs from 'dayjs';
 import { Session, SocialMedia } from 'hypicle';
 
-interface PlayerCardProps {
-  name: string;
-  firstLogin: number;
-  lastLogin: number;
-  level: number;
-  karma: number;
-  status: Session;
-  rank: string;
-  socials: SocialMedia;
-}
-
 const format = 'DD MMM YYYY HH:mm:ss';
+
+const socialMap = [
+  {
+    name: 'DISCORD',
+    title: 'Discord',
+    icon: IconBrandDiscord,
+    color: '#454FBF',
+  },
+  {
+    name: 'HYPIXEL',
+    title: 'Hypixel Forums',
+    icon: IconHexagonLetterH,
+    color: '#F4C75D',
+  },
+  {
+    name: 'TWITCH',
+    title: 'Twitch Channel',
+    icon: IconBrandTwitch,
+    color: '#6441A5',
+  },
+  {
+    name: 'TIKTOK',
+    title: 'TikTok',
+    icon: IconBrandTiktok,
+    color: '#00f2EA',
+  },
+  {
+    name: 'INSTAGRAM',
+    title: 'Instagram',
+    icon: IconBrandInstagram,
+    color: '#C13584',
+  },
+  {
+    name: 'YOUTUBE',
+    title: 'YouTube Channel',
+    icon: IconBrandYoutube,
+    color: '#FF0000',
+  },
+  {
+    name: 'TWITTER',
+    title: 'Twitter',
+    icon: IconBrandTwitter,
+    color: '#1DA1F2',
+  },
+];
 
 const useStyles = createStyles((theme) => ({
   card: {
@@ -42,6 +89,18 @@ const useStyles = createStyles((theme) => ({
     borderTop: `${rem(1)} solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
     }`,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+  socials: {
+    padding: theme.spacing.md,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  socialIcon: {
+    marginRight: theme.spacing.xs,
   },
 }));
 
@@ -49,7 +108,100 @@ const getAvatarByName = (username: string): string => {
   return `https://minotar.net/helm/${username}`;
 };
 
-// TODO: Socials section
+interface PlayerSocialsProps {
+  socials: SocialMedia;
+  onCopy: () => void;
+}
+
+const PlayerSocialLinks = ({ socials, onCopy }: PlayerSocialsProps) => {
+  const { classes, cx } = useStyles();
+  const [opened, { close, open }] = useDisclosure(false);
+
+  if (!socials.links || Object.keys(socials.links).length === 0) return null;
+
+  return Object.keys(socials.links).map((key) => {
+    const socialInfo = socialMap.find(
+      (item) => item.name === key.toUpperCase()
+    );
+
+    if (!socialInfo) return null;
+
+    const { title, color } = socialInfo;
+
+    return key === 'DISCORD' ? (
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+        <Popover
+          width={185}
+          position="top"
+          withArrow
+          shadow="md"
+          opened={opened}
+        >
+          <Popover.Target>
+            <CopyButton value={socials.links[key]}>
+              {({ copied, copy }) => (
+                <ActionIcon
+                  key={key}
+                  title={title}
+                  onClick={() => {
+                    copy();
+                    onCopy();
+                  }}
+                  onMouseEnter={open}
+                  onMouseLeave={close}
+                  className={classes.socialIcon}
+                >
+                  <socialInfo.icon style={{ color }} size="3rem" />
+                </ActionIcon>
+              )}
+            </CopyButton>
+          </Popover.Target>
+          <Popover.Dropdown
+            sx={{
+              pointerEvents: 'none',
+              position: 'absolute',
+              transform: 'translateY(-100%) translateX(-3.5%)',
+            }}
+          >
+            <Text fz="sm">
+              Discord:{' '}
+              <Text span fw={700}>
+                {socials.links[key]}
+              </Text>
+            </Text>
+            <Text fz="sm" fw={500}>
+              Click to Copy
+            </Text>
+          </Popover.Dropdown>
+        </Popover>
+      </div>
+    ) : (
+      <ActionIcon
+        component="a"
+        href={socials.links[key]}
+        target="_blank"
+        key={key}
+        title={title}
+        className={classes.socialIcon}
+      >
+        <socialInfo.icon style={{ color }} size="3rem" />
+      </ActionIcon>
+    );
+  });
+};
+
+interface PlayerCardProps {
+  name: string;
+  firstLogin: number;
+  lastLogin: number;
+  level: number;
+  karma: number;
+  status: Session;
+  rank: string;
+  socials: SocialMedia;
+  onCopy: () => void;
+}
+
 export default function PlayerProfileCard({
   name,
   firstLogin,
@@ -59,6 +211,7 @@ export default function PlayerProfileCard({
   status,
   rank,
   socials,
+  onCopy,
 }: PlayerCardProps) {
   const { classes, cx } = useStyles();
 
@@ -122,6 +275,12 @@ export default function PlayerProfileCard({
           {dayjs(lastLogin).format(format)}
         </Text>
       </Card.Section>
+
+      {(socials.links !== null || socials.links !== undefined) && (
+        <Card.Section className={classes.socials}>
+          <PlayerSocialLinks socials={socials} onCopy={onCopy} />
+        </Card.Section>
+      )}
     </Card>
   );
 }
